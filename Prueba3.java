8e3acdaeea05b82d15c729f6d2adf19f5d7ba176
sergiomartin <martinsergio444@gmail.com>
2024-03-12 13:32:29 +0100

    Cuarto commit

---
+++ Prueba3.java
@@ -0,0 +1,1 @@
+// Autor/a: Sergio Martin
---
+++ Prueba4.java
@@ -0,0 +1,11 @@
+public class Prueba4 {
+	int atributo = 0;
+	
+	public suma (int atributo) {
+		this.atributo = atributo; 
+	}
+	
+	public void setAtributo(int atributo) {
+		this.atributo = atributo;
+	}
+}
